e6c2dbb42e89d2985e0843ee729ef78608bb305c
nickmwangemi <nickmwangemi@gmail.com>
2021-05-01 12:08:08 +0300

    Added whitenoise

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -59,7 +59,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
